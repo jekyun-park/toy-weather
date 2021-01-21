4f8f319990f3e9d72daedb13572678b87786f718
jegyun-Park <parkjekyun@gmail.com>
2021-01-21 16:27:35 +0900

    weather application v0.1 released

--- App.js
+++ App.js
@@ -1,40 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {Alert} from "react-native";
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
-import * as Location from 'expo-location';
-import axios from 'axios';
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
 
 const API_KEY = "5f49b19b52e4aa8e25d176e9714fe552";
 
 export default class extends React.Component {
-  state  = {
-    isLoading : true
-  }
+  state = {
+    isLoading: true
+  };
   getWeather = async (latitude, longitude) => {
-    const {data} = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    const {
+      data: {
+        main: { temp },
+        weather,
+        name
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
-    console.log(data);
-  }
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp,
+      name: name
+    });
+  };
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {
-        coords : { latitude, longitude }
+        coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false })
     } catch (error) {
-      Alert.alert("Can't find you.","So sad");
-    } 
+      Alert.alert("Can't find you.", "So sad");
+    }
   };
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp, condition, name } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} name={name} />
+    );
   }
 }
--- Loading.js
+++ Loading.js
@@ -1,25 +1,35 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Loading() {
-    return (
+  return (
     <View style={styles.container}>
-        <Text style={styles.text}>Getting the Good Weather</Text>
+      <StatusBar barStyle="dark-content" />
+      <Text style={styles.text}>Getting Weather now ...</Text>
+      <View style={styles.halfcontainer}>
+        <Text style={styles.text}>
+          Hi, It's Jegyun's Toy Weather Application, Please wait a Moment...
+        </Text>
+      </View>
     </View>
-    );
-} 
-
+  );
+}
 
 const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        justifyContent : "flex-end",
-        paddingHorizontal : 30,
-        paddingVertical : 100,
-        backgroundColor : "#FDF6AA"
-    },
-    text : {
-        color : "#2c2c2c",
-        fontSize : 30
-    }  
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FDF6AA"
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 30
+  },
+  halfcontainer: {
+    flex: 1,
+    justifyContent: "center"
+  }
 });
